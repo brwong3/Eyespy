6b34905cab3ea801d75657aa41477c35ae801f39
brwong3 <brwong3@s.sfusd.edu>
2022-08-01 00:10:38 -0700

    Testing Out Command Creation

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ApplicationCommandOptionWithChoicesAndAutocompleteMixin } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds], partials: ['MESSAGE'] });
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
+
+client.on('messageDelete', message => {
+	console.log(message)
+})
 
 client.login(token);
